2c1f6f431eebe63a5b562f47d8a06f3eb76678ff
Dan-S-Lee <34869440+Dan-S-Lee@users.noreply.github.com>
2022-10-31 22:32:06 -0500

    Adding all linux data

---
+++ data/train/psudeocode.py
@@ -0,0 +1,15 @@
+
+line1 = [09:53] <crimsun> neoxed: thanks. Next time please read my entire request regarding a paste web site. :)
+line2 = [09:53] <neoxed> systems download "don't forget.tex" 23kb
+line3 = [09:54] <neoxed> ah :)
+
+preprocessing(data)
+
+feature1, feature2, feature3
+
+model(line1, line2) = 1
+
+line1, line2
+
+line1, line3
+
